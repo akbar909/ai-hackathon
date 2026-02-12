513b33579c8b90f27d3ba1c517d039e5cb314f0d
Ghulam Akbar <jamaliakbar909@gmail.com>
2026-02-13 01:29:21 +0500

    deployment

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://akbar909-ai-logistics-backend.hf.space';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
